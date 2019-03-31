9d6178054c78f4295662093861a05756cc0bb5b1
Henry Szeto <henryszeto@gmail.com>
2019-03-30 23:21:07 -0700

    setup Rspec with health check endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+
+  def liveness
+    logger.info "Alive....."
+    head 204
+  end
+
+  def readiness
+    # Ensure that database is connected
+    pg = !!ActiveRecord::Base.connection
+    
+    if pg
+      logger.info "Ready......"
+      head 204
+    else
+      logger.debug "Not Ready......"
+      render json:{errors: {pgdb: pg}}, status: 500
+    end
+  end
 end
---
+++ spec/requests/health_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Application', type: :request do
+  describe 'Liveness and Readiness' do
+    it 'System is alive' do
+      get '/liveness'
+
+      expect(response.status).to eq(204)
+    end
+
+    it 'System is ready' do
+      get '/readiness'
+
+      expect(response.status).to eq(204)
+    end
+
+    context 'When database is not ready' do
+      before do
+        allow( ActiveRecord::Base )
+          .to receive_message_chain("connection")
+          .and_return(false)
+      end
+
+      it 'return 500' do
+        get '/readiness'
+
+        expect( response.status ).to eq 500
+      end
+    end
+  end
+end
