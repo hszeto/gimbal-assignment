a735cf10edf6af5f095c2c34a88e16ae4a2b2321
Henry Szeto <henryszeto@gmail.com>
2019-03-31 00:40:55 -0700

    seed db with csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+CSV.foreach(Rails.root.join('db/cafes.csv'), headers: true) do |row|
+  Cafe.create!({
+    :name => row[0],
+    :lat => row[1],
+    :lon => row[2]
+  })
+
+  puts 'Row added.'
+end
