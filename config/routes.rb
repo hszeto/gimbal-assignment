2db4951f18bce47f685c010a731ecc89367e8c5f
Henry Szeto <henryszeto@gmail.com>
2019-03-31 01:31:49 -0700

    nearby locations controller and spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get '/liveness',  to: 'application#liveness'
   get '/readiness', to: 'application#readiness'
+
+  scope 'api' do
+    post 'nearby', to: 'location#nearby'
+  end
 end
